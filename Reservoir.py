1ce90909a1a2e72aa4126cab8e81354eb50eccfe
Mitchell Hillegeist <kwipso@gmail.com>
2020-02-23 15:16:19 -0800

    Added function for setting cell properties for rectangular regions of the reservoir. Currently doesn't use the value specified; needs to be fixed.

--- Reservoir.py
+++ Reservoir.py
@@ -3,9 +3,22 @@ from OpenGL.GL import *
 from OpenGL.GLU import *
 from OpenGL.GLUT import *
 import sys
-from math import sin,cos,sqrt,pi
+from math import sin,cos,sqrt,pi,floor
 from operator import add
 import Draw
+
+class ResRegion(object):
+	"""docstring for ResRegion."""
+
+	def __init__(self):
+		super(ResRegion, self).__init__()
+		self.iStart = 0
+		self.iEnd = 0
+		self.jStart = 0
+		self.jEnd = 0
+		self.kStart = 0
+		self.kEnd = 0
+
 
 class Cell(object):
 	def __init__(self):
@@ -22,15 +35,38 @@ class Reservoir(object):
 		self.height = height
 		self.depth = depth
 		self.cell_dim = cell_dim
-		self.grid = [[[0 for _ in range(width)] for _ in range(height)] for _ in range(depth)]
+		#Creates grid with a new defualt Cell for every element.
+		self.grid = [[[Cell() for _ in range(width)] for _ in range(height)] for _ in range(depth)]
 
-		for i in range(self.width-1):
-			for j in range(self.height-1):
-				for k in range(self.depth-1):
-					self.grid[i][j][k] = Cell()
-					if i % 2 == 1:
-						self.grid[i][j][k].oilPct = 0.0
-						self.grid[i][j][k].wtrPct = 100.0
+		region = ResRegion()
+		region.iStart = floor(width / 4)
+		region.iEnd = floor(width / 4 * 3)
+		region.jStart = floor(height / 4)
+		region.jEnd = floor(height / 4 * 3)
+		region.kStart = floor(depth / 4)
+		region.kEnd = floor(depth / 4 * 3)
+
+		self.SetCellsValue(region, "wtrPct", 100.0)
+		self.SetCellsValue(region, "oilPct", 0.0)
+
+		# for i in range(self.width-1):
+		# 	for j in range(self.height-1):
+		# 		for k in range(self.depth-1):
+		# 			#Set properties of the current cell.
+		# 			if i % 2 == 1:
+		# 				self.grid[i][j][k].oilPct = 0.0
+		# 				self.grid[i][j][k].wtrPct = 100.0
+
+	def SetCellsValue(self, region, source, value):
+		for i in range(region.iStart,region.iEnd):
+			for j in range(region.jStart,region.jEnd):
+				for k in range(region.kStart,region.kEnd):
+					#Set properties of the current cell.
+					cell = self.grid[i][j][k]
+					cell.oilPct = 0.0
+					cell.wtrPct = 100.0
+					# cell_prop = getattr(cell, source)
+					# cell_prop = value
 
 def DrawGrid(res):
 	red = (0.8, 0.1, 0.0, 1.0)
